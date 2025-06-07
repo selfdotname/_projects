1a71baceeda60f2337fc1aaddf710cc4dc47cbc0
obaro <dietakeobaro7@gmail.com>
2025-06-07 12:15:46 +0100

    update

--- remote-mysql/backend/app.js
+++ remote-mysql/backend/app.js
@@ -1,17 +1,26 @@
 import express from "express";
 import session from "express-session";
+import MySQLStore from "express-mysql-session";
 
 import { mysqlRouter } from "./Routes/mysqlRoutes.js";
 
+const options = {
+  host: "sql7.freesqldatabase.com",
+  user: "sql7783543",
+  password: "qY9JuD3Z7w",
+  database: "sql7783543",
+};
 const PORT = 3000;
 
 const app = express();
+const sessionStore = new MySQLStore(options);
 
 app.use(
   session({
     secret: "my-secret",
     resave: false,
     saveUninitialized: true,
+    store: sessionStore
   })
 );
 
