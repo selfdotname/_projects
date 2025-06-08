4820115a1c84cdd623434b31f0f44c571bad1f17
obaro <dietakeobaro7@gmail.com>
2025-06-08 01:36:05 +0100

    update

--- express-react/app.js
+++ express-react/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
-import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
 
 app.use(express.static("public"));
 
@@ -17,4 +21,6 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from Express.js" });
 });
 
-app.listen(PORT, () => console.log(`Server Live`));
+// app.listen(PORT, () => console.log(`Server Live`));
+
+export default app;
